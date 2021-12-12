7236ef8c17cd351b840be20d8cd98e9c7366bcdc
Sam King <mail@samking.co>
2021-12-12 02:07:59 +0000

    Add custom headers for API routes

--- next.config.js
+++ next.config.js
@@ -12,6 +12,28 @@ module.exports = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/api/seasons-greetings/image",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "image/svg+xml",
+          },
+        ],
+      },
+      {
+        source: "/api/seasons-greetings/animation_url",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/html",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
--- pages/api/seasons-greetings/image.ts
+++ pages/api/seasons-greetings/image.ts
@@ -3,12 +3,7 @@ import { makeCoverArt } from "tokens/cozyco-seasons-greetings/cover-art";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const seed = req.query.s as string;
-  // const from = req.query.f as string;
-  // const to = req.query.t as string;
-
   const svg = makeCoverArt(seed);
-
-  res.setHeader("Content-Type", "image/svg+xml");
   res.send(svg);
 };
 
