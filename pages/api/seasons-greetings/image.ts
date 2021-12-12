85cf4cdd2f2a18eb157afe48e8b8a1328edc88b9
Sam King <mail@samking.co>
2021-12-12 02:25:36 +0000

    Potential fix for image url

--- pages/api/seasons-greetings/image.ts
+++ pages/api/seasons-greetings/image.ts
@@ -4,7 +4,13 @@ import { makeCoverArt } from "tokens/cozyco-seasons-greetings/cover-art";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const seed = req.query.s as string;
   const svg = makeCoverArt(seed);
-  res.send(svg);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.setHeader(
+    "Cache-Control",
+    "public, immutable, no-transform, s-maxage=31536000, max-age=31536000"
+  );
+  res.end(svg);
 };
 
 export default handler;
